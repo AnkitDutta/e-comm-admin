be57df753ef50f2eae1f20f7034f2fceb2d98a11
AnkitDutta <39426251+AnkitDutta@users.noreply.github.com>
2023-10-15 12:31:28 +0530

    updated middleware.js

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,13 @@
 import { authMiddleware } from "@clerk/nextjs";
 
+// export default authMiddleware({
+//   publicRoutes: ["/api/:path*"],
+// });
+
 export default authMiddleware({
-  publicRoutes: ["/api/:path*"],
-});
+   publicRoutes: ['/', '/api/webhook/clerk'], 
+   ignoredRoutes: ['/api/webhook/clerk'], 
+  });
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
